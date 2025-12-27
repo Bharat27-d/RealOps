af6cda89243d429fa847333d9329e0b6b5d2f169
Bharat Digrase <bharatdigrase276@gmail.com>
2025-12-27 21:03:34 +0530

    1

--- bot/firebase.js
+++ bot/firebase.js
@@ -20,7 +20,7 @@ if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_PRIVATE_KEY || !pr
 
     app = admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
-      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`
+      databaseURL: "https://realops-777-default-rtdb.europe-west1.firebasedatabase.app"
     }, 'bot-instance');
   }
 
